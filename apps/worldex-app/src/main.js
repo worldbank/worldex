56a3122d6c02e9f4fc79ef8cea4378c806884263
Aivin V. Solatorio <avsolatorio@gmail.com>
2023-05-16 20:06:17 -0400

    Set up main
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- apps/worldex-app/src/main.js
+++ apps/worldex-app/src/main.js
@@ -4,6 +4,9 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 
+// Libraries
+import axios from 'axios'
+
 // Components
 import App from './App.vue'
 
@@ -13,8 +16,21 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 
+// Libraries
+import config from './config'
+
+// Styles
+import 'leaflet/dist/leaflet.css';
+
 const app = createApp(App)
 
 registerPlugins(app)
+
+// Register global properties after installing plugins
+app.config.globalProperties.$http = axios
+app.config.globalProperties.$window = window
+
+// Register custom config
+app.config.globalProperties.$config = config
 
 app.mount('#app')
