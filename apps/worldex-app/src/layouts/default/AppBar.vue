072d06703df997ebbbd669a197fa43ea3a9334de
Aivin V. Solatorio <avsolatorio@gmail.com>
2023-05-16 23:09:00 -0400

    Format bar header
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- apps/worldex-app/src/layouts/default/AppBar.vue
+++ apps/worldex-app/src/layouts/default/AppBar.vue
@@ -1,13 +1,14 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar flat style="background-color: #1460aa;">
     <v-app-bar-title>
-      <v-row>
+      <v-row style="color:white;">
         <v-col md="2">
           <h1><v-icon icon="mdi-circle-slice-6" />WorldEx</h1>
         </v-col>
         <v-col md="10">
-          <v-text-field class="main-search" v-model="search" append-inner-icon="mdi-magnify" label="Search" single-line
-            @keydown.enter="setSearch" @click:appendInner="setSearch" hide-details></v-text-field>
+          <v-text-field class="main-search" variant="outlined" v-model="search" append-inner-icon="mdi-magnify"
+            label="Search" single-line @keydown.enter="setSearch" @click:appendInner="setSearch"
+            hide-details></v-text-field>
         </v-col>
       </v-row>
     </v-app-bar-title>
