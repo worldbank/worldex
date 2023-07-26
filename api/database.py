0591cb62b45404fb00d3ad423fa3d0b3a872648b
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-07-26 14:21:53 +0800

    Initialize fastapi app

---
+++ api/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgresserver/db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
