7bed4db2a5a63fbacb07c45bc1ddfe5c016cf295
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-07-27 10:28:37 +0800

    Fix api image/container contexts

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@worldex/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host='0.0.0.0')
