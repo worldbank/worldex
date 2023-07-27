6e45033678258c61da9258f995f7a73d4de5278e
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-07-27 14:02:52 +0800

    Connect api to db

--- api/database.py
+++ api/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@worldex/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db/worldex"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- api/main.py
+++ api/main.py
@@ -1,12 +1,32 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
+from database import SessionLocal, engine
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally: 
+        db.close()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/h3_tiles/")
+def get_h3_tiles(db: Session = Depends(get_db)):
+    with engine.connect() as con:
+        return con.execute(text(
+            "SELECT i, h3_get_num_cells(i) FROM generate_series(0, 15, 3) i;"
+        ))
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000, host='0.0.0.0')
