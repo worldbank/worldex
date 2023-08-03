0489e6e8c58f9e7166bb339f850067c45eaada11
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-08-03 11:06:12 +0800

    Return h3 indices given a zxy extent

--- api/main.py
+++ api/main.py
@@ -21,9 +21,9 @@ async def root():
 
 
 @app.get("/h3_tiles/")
-async def get_h3_tiles():
-    results = await database.fetch_all("SELECT i, h3_get_num_cells(i) FROM generate_series(0, 15, 3) i;")
-    return [result[1] for result in results]
+async def get_h3_tiles(z: int, x: int, y: int):
+    results = await database.fetch_all(f"SELECT h3_polygon_to_cells(ST_Transform(ST_TileEnvelope({z}, {x}, {y}), 4326), 5)")
+    return {result[0] for result in results}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000, host='0.0.0.0')
