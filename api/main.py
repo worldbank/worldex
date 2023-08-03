ff60757d7593942745b0a1217d1f8ec6fc4282c3
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-08-03 12:18:06 +0800

    Use h3 resolution proportionate to z extent

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import Depends, FastAPI
 from databases import Database
+from services import get_h3_resolution
 
 app = FastAPI()
 database = Database("postgresql+asyncpg://worldex:postgres@db/worldex")
@@ -22,7 +23,10 @@ async def root():
 
 @app.get("/h3_tiles/")
 async def get_h3_tiles(z: int, x: int, y: int):
-    results = await database.fetch_all(f"SELECT h3_polygon_to_cells(ST_Transform(ST_TileEnvelope({z}, {x}, {y}), 4326), 5)")
+    h3_resolution = get_h3_resolution(z)
+    results = await database.fetch_all(f"""
+        SELECT h3_polygon_to_cells(ST_Transform(ST_TileEnvelope({z}, {x}, {y}), 4326), {h3_resolution});
+    """)
     return {result[0] for result in results}
 
 if __name__ == "__main__":
---
+++ api/services.py
@@ -0,0 +1,29 @@
+import math
+
+ZOOM_TO_H3_RESOLUTION = {
+    5: 1,
+    6: 2,
+    7: 3,
+    8: 3,
+    9: 4,
+    10: 5,
+    11: 6,
+    12: 6,
+    13: 7,
+    14: 8,
+    15: 9,
+    16: 9,
+    17: 10,
+    18: 10,
+    19: 11,
+    20: 11,
+    21: 12,
+    22: 13,
+    23: 14,
+    24: 15,
+}
+
+H3_RESOLUTION_TO_ZOOM = { v: k for k, v in ZOOM_TO_H3_RESOLUTION.items() }
+
+def get_h3_resolution(zoom: int) -> int:
+    return ZOOM_TO_H3_RESOLUTION.get(zoom, math.floor((zoom - 1) * 0.7))
