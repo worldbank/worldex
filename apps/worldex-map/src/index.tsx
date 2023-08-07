5415b54dbc4b85f48fd8f75cf427c0aa9eb04c99
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-08-07 14:13:03 +0800

    Initialize web map; migrate to vite

---
+++ apps/worldex-map/src/index.tsx
@@ -0,0 +1,33 @@
+/**
+ * Setup the global elements of the react app:
+ *  - redux store
+ *  - react router
+ *  - main component: App
+ */
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { createCartoSlice } from '@carto/react-redux';
+import App from './App';
+import { initialState } from 'store/initialStateSlice';
+import store from './store/store';
+// @ts-ignore
+import { setDefaultCredentials } from '@deck.gl/carto';
+
+// @ts-ignore
+store.injectReducer('carto', createCartoSlice(initialState));
+
+setDefaultCredentials({ ...initialState.credentials });
+
+const AppProvider = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(AppProvider);
