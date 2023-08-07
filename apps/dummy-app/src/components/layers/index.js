226db25e9f35e714cf33539f6e0e9aaba4021f5a
R G B <8906131+rgbongocan@users.noreply.github.com>
2023-08-07 16:07:27 +0800

    ui: Declutter the ui codebase (#11)
    * Rm worldex-app and dummy-app codebases
    * Move apps/worldex-map to simply ui

--- apps/dummy-app/src/components/layers/index.js
+++
@@ -1,9 +0,0 @@
-import H3Layer from './H3Layer';
-// [hygen] Import layers
-
-export const getLayers = () => {
-  return [
-    H3Layer(),
-    // [hygen] Add layer
-  ];
-};
--- apps/worldex-app/src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <!-- <HelloWorld /> -->
-  <Map />
-</template>
-
-<script setup>
-// import HelloWorld from '@/components/HelloWorld.vue'
-import Map from '../components/Map.vue';
-</script>
