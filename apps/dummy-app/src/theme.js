a2cdafffd13bc099ee450bf0fe4f63ad03d8392a
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-08-07 15:58:16 +0800

    Rm worldex-app and dummy-app codebases

--- apps/dummy-app/src/theme.js
+++
@@ -1,9 +0,0 @@
-import deepmerge from 'deepmerge';
-import { createTheme } from '@mui/material';
-import { cartoThemeOptions } from '@carto/react-ui';
-
-const customTheme = {};
-
-const theme = createTheme(deepmerge(cartoThemeOptions, customTheme));
-
-export default theme;
