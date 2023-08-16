581f3013399c17cccff4f3f9ab13dae532215d76
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-08-16 11:47:45 +0800

    Use pydantic v1 for compatibility with sqlmodel

--- api/app/database.py
+++
@@ -1,7 +0,0 @@
-from databases import Database
-from sqlalchemy.ext.declarative import declarative_base
-
-# TODO: get from settings
-database = Database("postgresql+asyncpg://worldex:postgres@db/worldex")
-
-Base = declarative_base()
