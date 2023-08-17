3ddaaf697b71ef81237e11daaf0ddf8b1079a34e
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-08-17 15:15:02 +0800

    Rework initial migration not to use sqlmodel

--- api/app/services.py
+++ api/app/services.py
@@ -1,4 +1,8 @@
 import math
+from typing import Iterator, Union
+
+from shapely.geometry.linestring import LineString
+from shapely.geometry.multilinestring import MultiLineString
 
 ZOOM_TO_H3_RESOLUTION = {
     5: 1,
@@ -29,3 +33,35 @@ H3_RESOLUTION_TO_ZOOM = {v: k for k, v in ZOOM_TO_H3_RESOLUTION.items()}
 def get_h3_resolution(zoom: int) -> int:
     zoom = math.floor(zoom)
     return ZOOM_TO_H3_RESOLUTION.get(zoom, 1 if zoom < 5 else 15)
+
+
+def sequential_deduplication(func: Iterator[str]) -> Iterator[str]:
+    '''
+    Decorator that doesn't permit two consecutive items to be the same
+    '''
+    def inner(*args):
+        iterable = func(*args)
+        last = None
+        while (cell := next(iterable, None)) is not None:
+            if cell != last:
+                yield cell
+            last = cell
+    return inner
+
+@sequential_deduplication
+def h3polyline(line: Union[LineString, MultiLineString], resolution: int) -> Iterator[str]:
+    '''
+    Iterator yielding H3 cells representing a (multi)line,
+    retaining order and self-intersections
+    '''
+    if line.geom_type == 'MultiLineString':
+        # Recurse after getting component linestrings from the multiline
+        for l in map(lambda geom: h3polyline(geom, resolution), line.geoms):
+            yield from l
+    else:
+        coords = zip(line.coords, line.coords[1:])
+        while (vertex_pair := next(coords, None)) is not None:
+            i, j = vertex_pair
+            a = h3.geo_to_h3(*i[::-1], resolution)
+            b = h3.geo_to_h3(*j[::-1], resolution)
+            yield from h3.h3_line(a, b) # inclusive of a and b
--- api/migrations/env.py
+++ api/migrations/env.py
@@ -8,6 +8,7 @@ from sqlmodel import SQLModel
 
 from alembic import context
 
+from app.db import Base
 from app.models import *
 
 # this is the Alembic Config object, which provides
@@ -23,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = SQLModel.metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
