1e7b106b48672c522c18f4dbd86966d408317447
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-09-05 10:34:02 +0800

    Drop unused btree index on h3_index

--- api/migrations/versions/02b869b43dee_create_indices_for_getting_h3_parent.py
+++ api/migrations/versions/02b869b43dee_create_indices_for_getting_h3_parent.py
@@ -17,12 +17,13 @@ down_revision: Union[str, None] = "31de72c3b38c"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
-index_name_prefix = "ix_h3_data_h3_index_parent"
 # for resolutions 5 to 7 we're already aggregating/counting datasets, thus not using h3_cell_to_parent
 h3_resolutions = [1, 2, 3, 4]
+index_name_prefix = "ix_h3_data_h3_index_parent"
 
 
 def upgrade() -> None:
+    op.drop_index(op.f("ix_h3_data_h3_index"), table_name="h3_data")
     for res in h3_resolutions:
         op.execute(
             f"CREATE INDEX {index_name_prefix}_{res} on h3_data (h3_cell_to_parent(h3_index, {res}));"
@@ -30,5 +31,6 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    for res in h3_resolutions:
-        op.execute(f"DROP INDEX {index_name_prefix}_{res};")
+    op.create_index(op.f("ix_h3_data_h3_index"), "h3_data", ["h3_index"], unique=False)
+    indices = ", ".join(f"{index_name_prefix}_{res}" for res in h3_resolutions)
+    op.execute(f"DROP INDEX {indices};")
