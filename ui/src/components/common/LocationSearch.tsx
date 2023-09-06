c6b19f6828b301e2398b2438abc913684baaf2ac
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-09-06 11:25:21 +0800

    Prettify the location search component

--- ui/src/components/common/LocationSearch.tsx
+++ ui/src/components/common/LocationSearch.tsx
@@ -1,16 +1,29 @@
-import { IconButton, Paper, TextField } from "@mui/material"
+import { CircularProgress, IconButton, Paper, TextField } from "@mui/material"
 import SearchIcon from '@mui/icons-material/Search';
-import React, { useState } from "react";
+import React, { MouseEventHandler, useState } from "react";
 import { setViewState } from '@carto/react-redux';
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "store/store";
 import { WebMercatorViewport } from '@deck.gl/core/typed';
 
+const searchAdornment = ({isLoading, handleClick}: {isLoading: boolean, handleClick: React.MouseEventHandler<HTMLButtonElement>}) =>
+  <div className="flex justify-center items-center w-[2em] mr-[-8px]">
+    {
+      isLoading ? <CircularProgress size="1.2em" /> : (
+        <IconButton aria-label="search" onClick={handleClick}>
+            <SearchIcon />
+        </IconButton>
+      )
+    }
+  </div>
+
 const LocationSearch = ({ className }: { className?: string }) => {
-  const [query, setQuery]= useState("");
+  const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
   const viewState = useSelector((state: RootState) => state.carto.viewState);
   const handleClick = async () => {
+    setIsLoading(true);
     const { width, height } = viewState;
     const resp = await fetch(
       `https://nominatim.openstreetmap.org/search?q=${query}&format=json`,
@@ -25,23 +38,25 @@ const LocationSearch = ({ className }: { className?: string }) => {
       // @ts-ignore
       dispatch(setViewState({...viewState, latitude, longitude, zoom }));
     }
+    setIsLoading(false);
   }
   return (
     <Paper className={className}>
       <div className="flex items-end">
         <TextField
-          label="Location"
+          label="Search Location"
           variant="outlined"
           value={query}
           onChange={
             (event: React.ChangeEvent<HTMLInputElement>) => {
-                setQuery(event.target.value);
+              setQuery(event.target.value);
             }
           }
+          InputProps={{
+            endAdornment: searchAdornment({ isLoading, handleClick }),
+            className: "pr-0.5"
+          }}
         />
-        <IconButton aria-label="search" onClick={handleClick}>
-          <SearchIcon />
-        </IconButton>
       </div>
     </Paper>
   )
