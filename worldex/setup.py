7d083ba4088d4b505e0a728e6d12264f321509f4
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-09-21 08:47:24 +0800

    Refactor handlers to be installable

---
+++ worldex/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="worldex",
+    version="0.0.1",
+    description="Handle geospatial data types and convert them into H3 indices",
+    # packages=['worldex', 'worldex.handlers'],
+    packages=find_packages(),
+    install_requires=[
+        "rasterio>=1.3.8,<2",
+        "h3ronpy>=0.17.4,<1",
+        "geopandas>=0.3.2,<1",
+    ],
+)
--- worldex/worldex/handlers/vector_handlers.py
+++ worldex/worldex/handlers/vector_handlers.py
@@ -35,9 +35,12 @@ class VectorHandler(BaseHandler):
     def from_file(cls, file: File, resolution: Optional[int] = None):
         if isinstance(file, str):
             file = Path(file)
-        if file.suffix == ".csv":
-            return cls.from_csv(file, resolution)
-        else:
+        try:
+            if file.suffix == ".csv":
+                return cls.from_csv(file, resolution)
+        except AttributeError:
+            pass
+        finally:
             gdf = gpd.read_file(file)
             return cls(gdf, resolution)
 
