26a04cb2459c1cfbcc5471569b924e643e817aab
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-09-25 16:37:23 +0800

    Rm comment

--- worldex/setup.py
+++ worldex/setup.py
@@ -4,7 +4,6 @@ setup(
     name="worldex",
     version="0.0.1",
     description="Handle geospatial data types and convert them into H3 indices",
-    # packages=['worldex', 'worldex.handlers'],
     packages=find_packages(),
     install_requires=[
         "rasterio>=1.3.8,<2",
