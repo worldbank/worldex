ba95b10273b5e1acf37d710380aa1b5f727e76aa
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-10-03 17:27:37 +0800

    Use functions

--- api/scripts/index_world_pop_compacted.py
+++ api/scripts/index_world_pop_compacted.py
@@ -5,6 +5,7 @@ import hashlib
 import s3fs
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 from sqlalchemy.sql import exists
 from app.models import Dataset, H3Data
 import sys
@@ -15,11 +16,41 @@ import h3
 import h3pandas
 import sqlalchemy
 from worldex.handlers.raster_handlers import RasterHandler
+from typing import Dict
 
 DATABASE_CONNECION = os.getenv("DATABASE_URL_SYNC")
 BUCKET = os.getenv("AWS_BUCKET")
 DATASET_DIR = os.getenv("AWS_DATASET_DIRECTORY")
-DATASET_NAME = "Nigeria Population Density"
+
+
+def create_dataset_from_metadata(metadata: Dict, sess: Session) -> Dataset:
+    dataset_name = metadata['name']
+    dataset_exists = sess.query(exists().where(Dataset.name == dataset_name)).scalar()
+    assert not dataset_exists, f"'{dataset_name}' dataset already exists" 
+
+    # TODO: actually create keyword objects
+    keywords: list[str] = metadata.pop("keywords")
+    # TODO: remove handling once parquet files are corrected
+    if "data_foramt" in metadata:
+        metadata["data_format"] = metadata.pop("data_foramt")
+    return Dataset(**metadata)
+
+
+def index_h3(file: s3fs.core.S3File, dataset_id: int, sess: Session):
+    tile_errors = 0
+    df_pop = pd.read_parquet(f)
+    df_pop = pd.DataFrame(
+        {'h3_index': list(h3.compact(df_pop['h3_index']))}
+    ).astype({'h3_index': str})
+    for _, row in df_pop.iterrows():
+        try:
+            sess.add(H3Data(h3_index=row['h3_index'], dataset_id=dataset_id))
+            sess.commit()
+        except sqlalchemy.exc.InternalError as e:
+            sess.rollback()
+            tile_errors += 1
+            continue
+    print("Tile errors:", tile_errors)
 
 
 def main():
@@ -35,44 +66,23 @@ def main():
     with Session() as sess:
         dirs = s3.ls("s3:///worldex-temp-storage/indexes/worldpop/")
         for dir in dirs:
-            if "zaf" not in dir[:57]:
-                continue
+            print(f"Indexing {dir}")
             try:
                 with s3.open(f"s3://{dir}/metadata.json") as f:
                     metadata = json.load(f)
-                    metadata['name'] += ' (compacted)'
-                    dataset_name = metadata['name']
-                    if sess.query(exists().where(Dataset.name == dataset_name)).scalar():
-                        print(f"'{dataset_name}' dataset already exists")
+                    try:
+                        dataset_pop = create_dataset_from_metadata(metadata, dataset_pop.id, sess)
+                    except AssertionError:
                         continue
-                    print(f"Indexing {dir}")
-                    keywords: list[str] = metadata.pop("keywords")
-                    if "data_foramt" in metadata:
-                        metadata["data_format"] = metadata.pop("data_foramt")
-                    dataset_pop = Dataset(**metadata)
                     sess.add(dataset_pop)
-                    sess.commit()
+                    # sess.commit()
                 with s3.open(f"s3://{dir}/h3.parquet") as f:
-                    tile_errors = 0
-                    df_pop = pd.read_parquet(f)
-                    df_pop = pd.DataFrame(
-                        {'h3_index': list(h3.compact(df_pop['h3_index']))}
-                    ).astype({'h3_index': str})
-                    for _, row in df_pop.iterrows():
-                        try:
-                            sess.add(H3Data(h3_index=row['h3_index'], dataset_id=dataset_pop.id))
-                            sess.commit()
-                        except sqlalchemy.exc.InternalError as e:
-                            sess.rollback()
-                            tile_errors += 1
-                            continue
-                    print("Tile errors:", tile_errors)
+                    index_h3(f)
             except Exception as e:
                 sess.delete(dataset_pop)
                 sess.commit()
                 raise e
-            finally:
-                sess.commit()
+            
 
 if __name__ == "__main__":
     sys.exit(main())
