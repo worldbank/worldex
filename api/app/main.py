4629ac5020ab03eeaeee31f3e501ed774075acd9
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-10-03 11:07:17 +0800

    Refactor query for compacted datasets

--- api/app/main.py
+++ api/app/main.py
@@ -54,10 +54,30 @@ async def get_h3_tiles(
         GROUP BY h3_index;
         """
     )
+    print(payload.resolution)
+    query = text(
+        """
+        WITH bbox AS (
+            SELECT ST_Transform(ST_TileEnvelope(:z, :x, :y), 4326) bbox
+        ),
+        uncompacted AS (
+            SELECT h3_uncompact_cells(array_agg(h3_index), :resolution) h3_index
+            FROM h3_data WHERE dataset_id = 77 AND h3_get_resolution(h3_index) < :resolution
+        ),
+        rest AS (
+            SELECT DISTINCT(h3_cell_to_parent(h3_index, :resolution)) h3_index FROM h3_data
+            WHERE dataset_id = 77 AND h3_get_resolution(h3_index) >= :resolution
+        )
+        SELECT * FROM uncompacted WHERE ST_WITHIN(h3_index::geometry, (SELECT bbox FROM bbox))
+        UNION
+        SELECT * FROM rest WHERE ST_WITHIN(h3_index::geometry, (SELECT bbox FROM bbox))
+        """
+    )
     query = query.bindparams(z=z, x=x, y=y, resolution=payload.resolution)
     results = await session.execute(query)
     return [
-        {"index": row[0], "dataset_count": row[1]}
+        # {"index": row[0], "dataset_count": row[1]}
+        {"index": row[0]}
         if should_count
         else {"index": row[0]}
         for row in results.fetchall()
