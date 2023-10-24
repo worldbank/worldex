c686036c58ecca9d1505365e21ad887cb44052ce
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-10-24 11:25:29 +0800

    Linearize without the aggregate views for now

--- api/migrations/versions/86b462787ba4_add_more_h3_and_dataset_indices.py
+++ api/migrations/versions/86b462787ba4_add_more_h3_and_dataset_indices.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "86b462787ba4"
-down_revision: Union[str, None] = "5cd5355da939"
+down_revision: Union[str, None] = "e126787e81fb"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
