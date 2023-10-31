2b9167c560255f28e27bcc87539a0fe8a1399f5d
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-10-31 15:19:57 +0800

    Add a close button

--- ui/src/components/common/WelcomeModal.tsx
+++ ui/src/components/common/WelcomeModal.tsx
@@ -1,5 +1,6 @@
-import { Box, Modal, Typography } from "@mui/material";
+import { Box, IconButton, Modal, Typography } from "@mui/material";
 import { useState } from "react";
+import CloseIcon from '@mui/icons-material/Close'
 
 const WelcomeModal = () => {
   const [open, setOpen] = useState(true);
@@ -8,12 +9,15 @@ const WelcomeModal = () => {
   return (
     <Modal disableAutoFocus open={open} onClose={handleClose}>
       <Box className="absolute inset-x-1/2 inset-y-1/2 -translate-x-1/2 -translate-y-1/2 w-[30rem] h-64 bg-neutral-50 p-4 rounded-md shadow-md">
+        <IconButton onClick={handleClose} className="absolute top-1 right-1">
+          <CloseIcon />
+        </IconButton>
         <Typography className="text-lg font-bold pb-2">Welcome to WorldEx!</Typography>
         <p className="pb-2">
           A global catalog of n socioeconomic geospatial data that leverages H3 indexing to improve subnational data discoverability.
         </p>
         <p className="pb-2">
-          We are constantly indexing new datasets and partnering with various organizations to broadedn the diversity of the datasets inxed in Worldex. Here are the latest updates.
+          We are constantly indexing new datasets and partnering with various organizations to broaden the diversity of the datasets indexed in Worldex. Here are the latest updates.
         </p>
       </Box>
     </Modal>
