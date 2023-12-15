e94075853048b0afe2a23118a73f4d68fea6fee8
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2023-12-15 14:30:26 +0800

    Adjust zoom-to-h3-resolution mapping - notably displaying resolution 2 tiles at minimum now

--- ui/src/constants/h3.ts
+++ ui/src/constants/h3.ts
@@ -1,11 +1,12 @@
 /* eslint-disable */
 export const ZOOM_H3_RESOLUTION_PAIRS = [
-    [1, 3],
+    [1, 2],
+    [2, 3],
     [3, 4],
-    [4, 5],
+    [5, 5],
     [6, 6],
     [8, 7],
-    [10, 8],
+    [9, 8],
 ];
 
 export const MINIMUM_ZOOM = Math.min(ZOOM_H3_RESOLUTION_PAIRS[0][0], 1);
