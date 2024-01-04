7a431c52ff4bdcaa2bf05b35718a70c2036a6037
R G B <8906131+rgbongocan@users.noreply.github.com>
2024-01-03 17:57:09 -0800

    ui: Make source org sections on the sidebar scrollable (#91)
    * Create a single layer for all resolution h3 tiles to avoid flickering when zooming in/out
    
    * Rm minZoom to avoid flickering when zooming out
    
    * Make sidebar section per source org scrollable; list dataset files on a table
    
    * Fix semantic usage of list
    
    * Separate files table into a component
    
    * Restore position:relative class on accordion

--- ui/src/components/common/sidebar/Datasets.tsx
+++ ui/src/components/common/sidebar/Datasets.tsx
@@ -1,10 +1,32 @@
 import ChevronRight from "@mui/icons-material/ChevronRight";
-import { Accordion, AccordionDetails, AccordionSummary, Box, Divider, IconButton, Link, List, ListItem, Popover, Stack, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Divider, IconButton, Link, List, ListItem, Popover, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useState } from "react";
 import { Dataset } from "../types";
 import { format } from "date-fns"
 
-const DatasetItem = ({idx, dataset}: {idx: number, dataset: Dataset}) => {
+
+const FilesTable = ({files}: {files: string[]}) => (
+  <>
+    <Typography className="text-sm">
+      <strong>Files:</strong>
+    </Typography>
+    <TableContainer className="max-w-100 overflow-x-scroll">
+      <Table size="small" aria-label="files table">
+        <TableBody>
+          {files.map((file: string, idx: number) => (
+            <TableRow>
+              <TableCell className="pl-0">
+                <Link href={file} target="_blank">{file}</Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </>
+)
+
+const DatasetItem = ({idx, dataset, className}: {idx: number, dataset: Dataset, className: string}) => {
   const [anchor, setAnchor] = useState(null);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchor(event.currentTarget);
@@ -13,9 +35,10 @@ const DatasetItem = ({idx, dataset}: {idx: number, dataset: Dataset}) => {
     setAnchor(null);
   };
   const open = Boolean(anchor);
+  const dateFormat = "yyyy MMM d";
 
   return (
-    <Stack direction="row" className="p-3 items-center justify-between">
+    <Stack direction="row" className={`p-3 items-center justify-between ${className}`}>
       <Box className="m-0">
         <Typography className="text-sm">{idx+1}. {dataset.name}</Typography>
       </Box>
@@ -37,11 +60,9 @@ const DatasetItem = ({idx, dataset}: {idx: number, dataset: Dataset}) => {
             {
               (dataset.date_start && dataset.date_end) && (
                 <Typography className="text-sm">
-                  <strong>Date:</strong>{" "}
-                  {format(dataset.date_start, "yyyy MMM d")}
-                  {" - "}
-                  {format(dataset.date_end, "yyyy MMM d")}
-                  </Typography>
+                  <strong>Date:</strong>
+                  {` ${format(dataset.date_start, dateFormat)} - ${format(dataset.date_end, dateFormat)}`}
+                </Typography>
               )
             }
             <Typography className="text-sm">
@@ -52,17 +73,7 @@ const DatasetItem = ({idx, dataset}: {idx: number, dataset: Dataset}) => {
               <strong>Source:</strong>{" "}<Link href={dataset.url} target="_blank" rel="noopener noreferrer">{dataset.url}</Link>
             </Typography>
           </div>
-          <Typography className="text-sm"><strong>Files:</strong>
-            <List className="text-xs">
-              {
-                dataset.files && dataset.files.map((file: string, idx: number) => (
-                  <ListItem key={idx} className="p-0">
-                    <Link href={file} target="_blank" rel="noopener noreferrer">{file}</Link>
-                  </ListItem>
-                ))
-              }
-            </List>
-          </Typography>
+          { dataset.files && <FilesTable files={dataset.files} />}
         </div>
       </Popover>
     </Stack>
@@ -73,25 +84,37 @@ const Datasets = ({ datasetsByOrgs }: { datasetsByOrgs: { [source_org: string]: 
   <div>
     { 
       Object.entries(datasetsByOrgs).map(([org, datasets]) => (
-        <Accordion disableGutters elevation={0} key={org} className="!relative">
+        <Accordion
+          disableGutters
+          elevation={0}
+          key={org}
+          className="!relative"
+        >
           <AccordionSummary>
             <Typography className="font-bold">{org}</Typography>
           </AccordionSummary>
           <Divider />
-          <AccordionDetails className="p-0">
+          <AccordionDetails
+            className="p-0"
+          >
+            <List className="m-0 p-0 max-h-[60vh] overflow-y-scroll">
             {
               datasets.map((dataset: Dataset, idx: number) => (
-                <List className="m-0 py-0 max-h-full overflow-y-auto">
-                  <ListItem className="p-0">
-                    <Stack className="w-full" spacing={1}>
-                      <DatasetItem idx={idx} dataset={dataset} />
-                      {idx + 1 < datasets.length && <Divider />}
-                    </Stack>
+                <>
+                  <ListItem key={idx} className="p-0">
+                      <DatasetItem idx={idx} dataset={dataset} className="w-full" />
                   </ListItem>
-                </List>
+                  {idx + 1 < datasets.length && (
+                    <ListItem className="p-0">
+                      <Divider className="w-full" />
+                    </ListItem>
+                  )}
+                </>
               ))
             }
+            </List>
           </AccordionDetails>
+          <Divider />
         </Accordion>
       ))
     }
