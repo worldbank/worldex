3ba27181075f5d20529d630c410e5495e54d9949
R G B <8906131+rgbongocan@users.noreply.github.com>
2024-01-15 23:27:40 -0800

    fix: Deduplicate resolution-2 h3 tiles (#98)
    * Update hdx metadata with script
    
    * Rm commented out code
    
    * Deduplicate h3 tiles on fill (sub)query at resolution 2
    
    * Reorganize queries

--- api/app/sql/dataset_coverage.py
+++ api/app/sql/dataset_coverage.py
@@ -1,13 +1,7 @@
-query = """
-WITH bbox AS (
-  SELECT ST_Transform(ST_TileEnvelope(:z, :x, :y), 4326) bbox
-),
-_fill AS (
-  SELECT h3_polygon_to_cells((SELECT bbox FROM bbox), CASE WHEN :resolution = 2 THEN 3 ELSE :resolution END) fill_index
-),
-fill AS (
-  SELECT DISTINCT CASE WHEN :resolution = 2 THEN h3_cell_to_parent(fill_index, 2) ELSE fill_index END fill_index FROM _fill
-),
+from .bbox_fill import FILL, FILL_RES2
+
+DATASET_COVERAGE = """
+WITH fill AS ({fill_query}),
 with_parents AS (
   SELECT fill_index, ARRAY_AGG(h3_cell_to_parent(fill_index, resolution)) parents
   FROM fill,
