f5f5aedda876b227909b52bd56f501f0286bd9b8
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2024-02-05 17:09:11 +0800

    Explore piping ZipLoader output to ShapefileLoader

--- ui/src/components/layers/PreviewLayer.tsx
+++ ui/src/components/layers/PreviewLayer.tsx
@@ -4,12 +4,41 @@ import { GeoJsonLayer } from '@deck.gl/layers/typed';
 // import { CartoLayer } from '@deck.gl/carto';
 import { RootState } from 'store/store';
 import { SHPLoader, ShapefileLoader } from '@loaders.gl/shapefile';
-// import { ZipLoader } from '@loaders.gl/zip';
-import { load } from '@loaders.gl/core';
+import { ZipLoader } from '@loaders.gl/zip';
+import { load, parse } from '@loaders.gl/core';
 import { useEffect, useState } from 'react';
 import '@loaders.gl/polyfills';
+import type { BinaryGeometry, Geometry } from '@loaders.gl/schema';
+import { binaryToGeometry } from '@loaders.gl/gis';
 
 export const PREVIEW_LAYER_ID = 'previewLayer';
+
+type Feature = any;
+
+const joinProperties = (geometries: Geometry[], properties: object[]): Feature[] => {
+  const features: Feature[] = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < geometries.length; i++) {
+    const geometry = geometries[i];
+    const feature: Feature = {
+      type: 'Feature',
+      geometry,
+      // properties can be undefined if dbfResponse above was empty
+      properties: (properties && properties[i]) || {},
+    };
+    features.push(feature);
+  }
+
+  return features;
+};
+
+const parseGeometries = (geometries: BinaryGeometry[]): Geometry[] => {
+  const geojsonGeometries: any[] = [];
+  for (const geom of geometries) {
+    geojsonGeometries.push(binaryToGeometry(geom));
+  }
+  return geojsonGeometries;
+};
 
 export default function PreviewLayer() {
   const { previewLayer } = useSelector((state: RootState) => state.carto.layers);
@@ -18,30 +47,30 @@ export default function PreviewLayer() {
   useEffect(() => {
     console.log('setting data');
     setData(
-      // load(
-      //   'https://cors-anywhere.herokuapp.com/data.humdata.org/dataset/e444670e-1766-4a32-b913-25fc8488ddd8/resource/889762d6-31ea-436c-b9ac-1ca589ce5196/download/khm_rdsl_gov.zip',
-      //   ZipLoader,
-      // // @ts-ignore
-      // ).then((d) => {
-      //   // const foo = 'bar';
-      //   console.log('unzipped', d);
-      //   return load('https://cors-anywhere.herokuapp.com/raw.githubusercontent.com/visgl/loaders.gl/master/modules/shapefile/test/data/graticules-and-countries/99bfd9e7-bb42-4728-87b5-07f8c8ac631c2020328-1-1vef4ev.lu5nk.shp', [ShapefileLoader], { shapefile: { shape: 'geojson-table' } });
-      //   // return parse(d, ShapefileLoader);
-      //   // console.log('ahhh');
-      //   // console.log('!', d);
-      //   // return load(d['khm_rdsl_gov.shp'], SHPLoader);
-      // }).then((d) => {
-      //   console.log('shape-loaded', d);
-      //   return d as Table;
-      //   // console.log('boooh');
-      //   // console.log('~', d);
-      //   // return d.geometries[0];
-      // }),
       load(
-        'https://raw.githubusercontent.com/visgl/deck.gl-data/master/test-data/shapefile/geo_export_14556060-0002-4a9e-8ef0-03da3e246166.shp',
-        [ShapefileLoader],
-        { worker: false, shapefile: { shape: 'geojson-table' } },
-      ),
+        'https://cors-anywhere.herokuapp.com/data.humdata.org/dataset/7ae5e7fb-7754-4a5c-8fc0-2fef0f60cfa0/resource/814d9449-37a2-488f-8de4-205c64b43ea3/download/djiroad2001ignundp.zip',
+        ZipLoader,
+      // @ts-ignore
+      ).then((d) => {
+        // const foo = 'bar';
+        console.log('unzipped', d);
+        // @ts-ignore
+        return parse(d['DJI_Road_2001_IGN_UNDP.shp'], [SHPLoader], { shapefile: { shape: 'geojson-table' } });
+      }).then((d: object) => {
+        console.log(d);
+        // @ts-ignore
+        const geometries = parseGeometries(d.geometries);
+        const features = joinProperties(geometries, []);
+        const ret = {
+          // @ts-ignore
+          header: d.header,
+          features,
+          shape: 'geojson-table',
+          type: 'FeatureCollection',
+        };
+        console.log(ret);
+        return ret;
+      }),
     );
   }, []);
 
