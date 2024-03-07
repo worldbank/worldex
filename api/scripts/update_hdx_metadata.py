ab82b2753a514d9632172b90401abcf8ce03c143
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2024-03-07 16:57:53 +0800

    Piggyback: rm unused function

--- api/scripts/update_hdx_metadata.py
+++ api/scripts/update_hdx_metadata.py
@@ -52,16 +52,6 @@ def update_dataset_metadata(
     return None
 
 
-def create_h3_indices(file: s3fs.core.S3File, dataset_id: int) -> List[H3Data]:
-    indices = pd.read_parquet(file)["h3_index"]
-    indices = list(indices)
-    df_pop = pd.DataFrame({"h3_index": indices}).astype({"h3_index": str})
-    return [
-        H3Data(h3_index=row["h3_index"], dataset_id=dataset_id)
-        for _, row in df_pop.iterrows()
-    ]
-
-
 def main():
     engine = create_engine(DATABASE_CONNECION)
 
