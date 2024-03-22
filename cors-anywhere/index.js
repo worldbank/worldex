0932c7d29ed6d046c865f23c39d0945236841617
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2024-03-22 18:13:21 +0800

    Create cloudbuild.yaml; fix host and port variable references

--- cors-anywhere/index.js
+++ cors-anywhere/index.js
@@ -5,9 +5,9 @@ var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
+    originWhitelist: process.env.ORIGIN_WHITELIST.split(",") || [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(PORT, HOST, function() {
+}).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
