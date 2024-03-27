3d7b17e84fb7bb3a5e8410ffb4933df9c1719b49
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2024-03-27 10:44:37 +0800

    Defer rendering of resolution 6 tiles from z-index 6 to 7

--- ui/src/constants/h3.ts
+++ ui/src/constants/h3.ts
@@ -4,9 +4,9 @@ export const ZOOM_H3_RESOLUTION_PAIRS: [number, number][] = [
     [2, 3],
     [3, 4],
     [5, 5],
-    [6, 6],
+    [7, 6],
     [8, 7],
-    [9, 8],
+    [10, 8],
 ];
 
 export const MINIMUM_ZOOM = Math.min(ZOOM_H3_RESOLUTION_PAIRS[0][0], 1);
