258192788354c8c13438f4a0df8757f10e924f1f
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2024-05-03 16:18:19 +0800

    Accomodate duplicate datasets on new dataset counts query (unfiltered)

--- api/app/sql/dataset_counts.py
+++ api/app/sql/dataset_counts.py
@@ -6,31 +6,44 @@ WITH fill AS ({fill_query}),
 with_parents AS (
   SELECT fill_index, ARRAY[{parents_array}] parents FROM fill GROUP BY fill_index
 ),
-parent_datasets AS (
+_parent_datasets AS (
   SELECT fill_index, dataset_id FROM with_parents
   JOIN h3_data ON h3_index = ANY(parents)
 ),
-children_datasets AS (
+parent_datasets AS (
+  SELECT
+    fill_index,
+    ARRAY_REMOVE(ARRAY_CAT(ARRAY[p.dataset_id], ARRAY_AGG(d.id)), NULL) dataset_ids
+  FROM _parent_datasets p
+  LEFT JOIN datasets d ON p.dataset_id = d.dataset_id
+  GROUP BY fill_index, p.dataset_id
+),
+p_agg AS (
+  SELECT fill_index, SUM(ARRAY_LENGTH(dataset_ids, 1)) dataset_count
+  FROM parent_datasets
+  GROUP BY fill_index
+),
+_children_datasets AS (
   SELECT fill_index, dataset_id FROM fill
-  JOIN h3_children_indicators ON h3_index = fill_index
+  JOIN h3_children_indicators c ON h3_index = fill_index
+),
+children_datasets AS (
+  SELECT fill_index, ARRAY_REMOVE(ARRAY_CAT(ARRAY[c.dataset_id], ARRAY_AGG(d.id)), NULL) dataset_ids FROM _children_datasets c
+  LEFT JOIN datasets d ON c.dataset_id = d.dataset_id
+  GROUP BY fill_index, c.dataset_id
+),
+c_agg AS (
+  SELECT fill_index, SUM(ARRAY_LENGTH(dataset_ids, 1)) dataset_count
+  FROM children_datasets
+  GROUP BY fill_index
 )
-SELECT
-  fill_index index,
-  ARRAY_LENGTH(ARRAY_REMOVE(
-    ARRAY_CAT(
-      ARRAY_CAT(
-        ARRAY_AGG(DISTINCT p.dataset_id),
-        ARRAY_AGG(DISTINCT c.dataset_id)
-      ),
-      ARRAY_AGG(DISTINCT dupe.id)
-    ),
-    NULL
-  ), 1)
-  dataset_count
-FROM parent_datasets p
-FULL JOIN children_datasets c USING (fill_index)
-LEFT JOIN datasets dupe ON p.dataset_id = dupe.dataset_id OR c.dataset_id = dupe.dataset_id
-GROUP BY fill_index
+SELECT * FROM (
+  SELECT
+    fill_index index,
+    (COALESCE(p.dataset_count, 0) + COALESCE(c.dataset_count, 0)) dataset_count
+  FROM p_agg p
+  FULL JOIN c_agg c USING (fill_index)
+) f WHERE dataset_count > 0;
 """
 
 DATASET_COUNTS_FILTERED = """
