1bbf3aee6c8914781e7754bb5c082b67f89fb347
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2024-05-22 11:40:52 +0800

    Distinguish united states' datasets from its minor outlying islands'

--- api/scripts/tag_h3_duplicate_datasets.py
+++ api/scripts/tag_h3_duplicate_datasets.py
@@ -29,11 +29,16 @@ def main():
     Session = sessionmaker(bind=engine)
     with Session() as sess:
         for country in COUNTRY_KEYWORDS:
+            natl_boundary_country = (
+                "national boundaries.*{country}"
+                if country != "united states"
+                else "national boundaries.*{country}(?! minor outlying islands)"
+            )
             q = text(
                 """
                 WITH country_datasets AS (
                     SELECT id, name FROM datasets
-                    WHERE name ILIKE :country
+                    WHERE name ~* :natl_boundary_country
                 ),
                 with_h3_counts AS (
                     SELECT
@@ -50,7 +55,7 @@ def main():
                 WHERE h3_count = (SELECT h3_count FROM with_h3_counts WHERE name ILIKE '%national boundaries%')
                 ORDER BY CASE WHEN name ILIKE '%national boundaries%' THEN 0 ELSE 1 end;
                 """
-            ).bindparams(country=f"%{country}%")
+            ).bindparams(natl_boundary_country=natl_boundary_country)
             results = sess.execute(q).fetchall()
             country_datasets = [row._mapping for row in results]
             print(country_datasets)
