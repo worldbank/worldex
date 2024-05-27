ac71812f4fa49cb8619b33728316ee4cc923c68a
Aivin V. Solatorio <avsolatorio@gmail.com>
2024-05-26 23:29:04 -0400

    Use the HF space query parser
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- api/app/search/parser.py
+++ api/app/search/parser.py
@@ -1,75 +1,30 @@
-import pycountry
-from gliner import GLiNER
-import os
+from gradio_client import Client
 
 
-_MODEL = None
-CACHE_DIR = os.environ.get("CACHE_DIR", None)
-GLINER_MODEL = os.environ.get("GLINER_MODEL", "urchade/gliner_base")
-
-print(f"Cache directory: {CACHE_DIR}")
+# Initialize the client
+client = Client("avsolatorio/query-parser")
 
 
-def get_model():
-    """Get the GLiNER model.
-
-    Returns:
-        GLiNER: The GLiNER model
-
-    """
-
-    global _MODEL
-    if _MODEL is None:
-        _MODEL = GLiNER.from_pretrained(GLINER_MODEL, cache_dir=CACHE_DIR)
-
-    return _MODEL
-
-
-def get_country(country_name: str):
-    """Get the normalized country information from the country name.
-
-    This function uses the pycountry library to search for the country name.
-
-    Args:
-        country_name (str): The country name
-
-    Returns:
-        pycountry.db.Country: The normalized country information
-
-    """
-
-    try:
-        return pycountry.countries.search_fuzzy(country_name)
-    except LookupError:
-        return None
-
-
-def parse_query(query: str, labels: list, threshold: float = 0.8):
-    """Parse the query and extract entities.
+def parse_query(query: str, labels: list, threshold: float = 0.3, nested_ner: bool = False):
+    """Parse the query and extract entities. This function is a wrapper for the Gradio API
+    running on the Hugging Face Space: https://huggingface.co/spaces/avsolatorio/query-parser.
 
     Args:
         query (str): The query
         labels (list): The list of labels to extract
-        threshold (float, optional): The threshold for extraction. Defaults to 0.8.
+        threshold (float, optional): The threshold for extraction. Defaults to 0.3.
 
     Returns:
         dict: The parsed query and extracted entities
 
     """
 
-    model = get_model()
+    labels = ",".join(labels)
 
-    _entities = model.predict_entities(query, labels, threshold=threshold)
-
-    entities = []
-
-    for entity in _entities:
-        if entity["label"] == "country":
-            country = get_country(entity["text"])
-            if country:
-                entity["normalized"] = [dict(c) for c in country]
-                entities.append(entity)
-        else:
-            entities.append(entity)
-
-    return {"query": query, "entities": entities}
+    return client.predict(
+            query=query,
+            labels=labels,
+            threshold=threshold,
+            nested_ner=nested_ner,
+            api_name="/parse_query"
+    )
