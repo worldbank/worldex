6835625e3e987221ff8234d7ba62b5bfc3577181
Aivin V. Solatorio <avsolatorio@gmail.com>
2024-05-26 23:39:20 -0400

    Update labels and threshold add nested_ner param
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- api/app/routers/search.py
+++ api/app/routers/search.py
@@ -11,9 +11,9 @@ router = APIRouter(
 )
 
 @router.get("/parse")
-def parse(query: Union[str, None] = None, labels: list[str] = Query(["year", "country"], description="A set of labels for tagging segments of the query."), threshold: float = 0.8):
+def parse(query: Union[str, None] = None, labels: list[str] = Query(["year", "country", "statistical indicator", "region"], description="A set of labels for tagging segments of the query."), threshold: float = 0.3, nested_ner: bool = False):
 
     if query is None:
         return {"error": "Query parameter is required."}
 
-    return parse_query(query, labels, threshold)
+    return parse_query(query, labels, threshold, nested_ner)
