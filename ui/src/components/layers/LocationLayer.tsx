a08984758d126aabbf780a1463dfa0ad422ea8c3
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2024-06-20 08:52:49 -0400

    Address todo: rename search layer into location layer

--- ui/src/components/layers/LocationLayer.tsx
+++ ui/src/components/layers/LocationLayer.tsx
@@ -2,17 +2,16 @@ import { useSelector } from 'react-redux';
 import { GeoJsonLayer } from '@deck.gl/layers/typed';
 import { RootState } from 'store/store';
 
-// TODO: rename to location layer?
-export const SEARCH_LAYER_ID = 'searchLayer';
+export const LOCATION_LAYER_ID = 'locationLayer';
 
-export default function SearchLayer() {
-  const { searchLayer } = useSelector((state: RootState) => state.carto.layers);
+export default function LocationLayer() {
+  const { locationLayer } = useSelector((state: RootState) => state.carto.layers);
   const { location } = useSelector((state: RootState) => state.search);
   const data = location?.geojson;
 
-  if (searchLayer && data) {
+  if (locationLayer && data) {
     return new GeoJsonLayer({
-      id: SEARCH_LAYER_ID,
+      id: LOCATION_LAYER_ID,
       data,
       pickable: true,
       stroked: true,
